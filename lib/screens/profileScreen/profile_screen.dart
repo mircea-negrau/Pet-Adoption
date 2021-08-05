18038c939856a20a448815eea60fa39e4d3e4cef
Mircea <mircea.negrau1@gmail.com>
2021-08-05 11:12:05 +0300

    Number of pet posts and shares added to the profile screen. Design improved.

--- lib/screens/profileScreen/profile_screen.dart
+++ lib/screens/profileScreen/profile_screen.dart
@@ -22,83 +22,169 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: widget.isDrawerOpen() == true
+                    ? Icon(Icons.arrow_forward_outlined,
+                        size: 30.0, color: Colors.grey.shade700)
+                    : Icon(Icons.arrow_back_outlined,
+                        size: 30.0, color: Colors.grey.shade700),
+                onPressed: () {
+                  widget.isDrawerOpen() == true
+                      ? widget.closeDrawer()
+                      : widget.openDrawer();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.edit, size: 30.0, color: Colors.grey.shade700),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(48, 10, 18, 0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(widget.user.photoUrl!),
+                    radius: 40.0,
+                  ),
+                  const SizedBox(width: 20.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.user.userName!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26.0,
+                        ),
+                      ),
+                      Text(
+                        "Joined 2021",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          wordSpacing: 3.0,
+                          fontSize: 14.0,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 25.0),
+              Row(
+                children: [
+                  Icon(Icons.phone, color: Colors.grey[400]),
+                  const SizedBox(width: 25.0),
+                  Text(
+                    "+40 774 015 082",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12.0),
+              Row(
+                children: [
+                  Icon(Icons.mail, color: Colors.grey[400]),
+                  const SizedBox(width: 25.0),
+                  Text(
+                    widget.user.email,
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 26.0),
+        Container(
+          height: 1.0,
+          color: Colors.grey[400],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(
-              icon: widget.isDrawerOpen() == true
-                  ? Icon(Icons.arrow_forward_outlined,
-                      size: 30.0, color: Colors.grey.shade700)
-                  : Icon(Icons.arrow_back_outlined,
-                      size: 30.0, color: Colors.grey.shade700),
-              onPressed: () {
-                widget.isDrawerOpen() == true
-                    ? widget.closeDrawer()
-                    : widget.openDrawer();
-              },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    "26",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  Text(
+                    "Shares",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            IconButton(
-              icon: Icon(Icons.edit, size: 30.0, color: Colors.grey.shade700),
-              onPressed: () {},
+            SizedBox(
+              height: 90,
+              child: VerticalDivider(
+                color: Colors.grey[400],
+                thickness: 1.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    "3",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  Text(
+                    "Pets",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      Padding(
-        padding: const EdgeInsets.fromLTRB(48, 10, 18, 0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(widget.user.photoUrl!),
-                  radius: 40.0,
-                ),
-                const SizedBox(width: 20.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      widget.user.userName!,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 26.0,
-                      ),
-                    ),
-                    Text(
-                      "Member since 2021",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12.0,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(height: 25.0),
-            Row(
-              children: const [
-                Icon(Icons.phone),
-                SizedBox(width: 25.0),
-                Text("+40 774 015 082"),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.mail),
-                SizedBox(width: 25.0),
-                Text(widget.user.email),
-              ],
-            )
-          ],
+        Container(
+          height: 1.0,
+          color: Colors.grey[400],
         ),
-      )
-    ]);
+      ]),
+    );
   }
 }
